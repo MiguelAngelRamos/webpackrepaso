512dc38fb83dc27c792a16757a754a084485d639
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-19 10:36:43 -0400

    fin de configuracion de webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,12 @@ const HtmlWebpack = require('html-webpack-plugin');
 const MiniCssExtract = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Plugin para minificar los archivos CSS en producción
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
+
 module.exports = {
-  mode: 'developer',
+  mode: 'production',
   output: {
     clean: true
   },
@@ -28,7 +32,25 @@ module.exports = {
       {
         test:/\.(png|jpe?g|gif)$/,
         loader: 'file-loader'
+      },
+      {
+        test:/\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'] // transpila codigo de ES6+(2017) ES12 A ES5 (2016)
+          }
+        }
       }
+
+    ]
+  },
+  optimization: {
+    minimize: true, // habilita la minimización de los archivos de salida
+    minimizer: [
+      new CssMinimizer(),
+      new Terser(),
     ]
   },
   plugins: [
